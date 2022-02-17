2ffef2eae7670c8988ae799426bd8c7d55a68124
Allegris <AstridChristiansenCS@gmail.com>
2022-02-17 10:18:59 +0100

    Skew SA12 + alphabet works

--- skew.py
+++ skew.py
@@ -1,5 +1,5 @@
-s = "mississippi$"
-SENTINEL = "#"
+s = "mississippi"
+SENTINEL = "$"
 
 # Make sure we don't reach beyond string length when getting a char from the string
 def safe_get_char(string, index):
@@ -15,10 +15,10 @@ def get_triplet(string, i):
 
 
 # Get alphabet of lex-names for the triples starting at given indices
-# Input should be string and suffix array
+# Input should be string and suffix array (sa_12)
 def get_alphabet(string, indices):
-	alphabet = {('#', '#', '#', 0)}
-	letter = 0
+	alphabet = {('$', '$', '$'): 0}
+	letter = 1
 	for i in indices:
 		triplet = get_triplet(string, i)
 		if triplet not in alphabet:
@@ -48,6 +48,35 @@ def map_u_to_string(i, m):
 		return 1 + 3 * i
 	else:
 		return 2 + 3 * (i - m - 1)
+
+def compute_sa_12(string):
+	triples_12 = []
+	for i in range(len(string)):
+		if i % 3 != 0:
+			triplet = get_triplet(string, i)
+			triples_12.append((i,) + triplet)
+	triples_12 = sorted(tuple(triples_12), key = lambda tup: (tup[1], tup[2], tup[3]))
+	sa_12 = [t[0] for t in triples_12]
+	return sa_12
+
+
+def skew_rec(string):
+	sa_12 = compute_sa_12(string)
+	alphabet = get_alphabet(string, sa_12)
+	return alphabet
+
+
+
+print(skew_rec(s))
+#print(get_alphabet(s, [11,10,7,1,4,8,2,5]))
+
+
+
+#def radix3(triple_list):
+
+
+
+
 
 
 
