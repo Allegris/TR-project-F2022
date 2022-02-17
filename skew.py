135275a8959b1466c4ccfeb165cde184b3c625a9
Allegris <AstridChristiansenCS@gmail.com>
2022-02-17 09:19:29 +0100

    Skew construct u string

--- skew.py
+++ skew.py
@@ -26,3 +26,30 @@ def get_alphabet(string, indices):
 			letter += 1
 	return alphabet
 
+
+# The u string consists of:
+# Lex names for i mod 3 = 1, SENTINEL, lex names for i mod 3 = 2
+def construct_u(string, alphabet):
+	u = ""
+	# Lex names for i mod 3 = 1
+	for  i in range(1, len(string), 3):
+		u += alphabet[get_triplet(string, i)]
+	# Add sentinel
+	u += SENTINEL
+	# Lex names for i mod 3 = 2
+	for  i in range(2, len(string), 3):
+		u += alphabet[get_triplet(string, i)]
+	return u
+
+
+# Map indices in u back to indices in the original input string
+def map_u_to_string(i, m):
+	if i < m:
+		return 1 + 3 * i
+	else:
+		return 2 + 3 * (i - m - 1)
+
+
+
+
+
