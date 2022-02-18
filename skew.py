e1a5d2cf2bcc85dfbd2451dfb4372d8d422f72ec
Allegris <AstridChristiansenCS@gmail.com>
2022-02-18 10:03:50 +0100

    Skew algo done!

--- skew.py
+++ skew.py
@@ -1,27 +1,33 @@
-s = "mississippi$" # must not include sentinel
+'''
+	Skew algorithm for computing the suffix array for a string in time O(n)
+'''
+
+
 SENTINEL_terminal = "$"
 SENTINEL_central = "#"
 
-# Make sure we don't reach beyond string length when getting a char from the string
+
+# If we reach beyond string, return terminal sentinel ("pad" string with terminal sentinel)
 def safe_get_char(string, index):
 	if index >= len(string):
 		return SENTINEL_terminal
 	else:
 		return string[index]
 
+
 # Get triplet starting at pos i in string
 def get_triplet(string, i):
 	return (safe_get_char(string, i), safe_get_char(string, i + 1), safe_get_char(string, i + 2))
 
 
-
 # Get alphabet of lex-names for the triples starting at given indices
-# Input should be string and suffix array (sa_12)
+# Input should be a string and suffix array (sa_12)
 def get_alphabet(string, indices):
-	alphabet = {('$', '$', '$'): 0}
+	alphabet = {('$', '$', '$'): 0} # never used: but gives alphabet correct size
 	letter = 1
 	for i in indices:
 		triplet = get_triplet(string, i)
+		# If triplet not seen before, add it to the alphabet with new, incremented lex name
 		if triplet not in alphabet:
 			alphabet[triplet] = letter
 			letter += 1
@@ -29,7 +35,7 @@ def get_alphabet(string, indices):
 
 
 # The u string consists of:
-# Lex names for i mod 3 = 1, SENTINEL, lex names for i mod 3 = 2
+# Lex names for i mod 3 = 1, CENTRAL SENTINEL, lex names for i mod 3 = 2
 def construct_u(string, alphabet):
 	u = ""
 	# Lex names for i mod 3 = 1
@@ -45,97 +51,107 @@ def construct_u(string, alphabet):
 
 # Map indices in u back to indices in the original input string
 def map_u_to_string_index(i, m):
+	# Left of the central sentinel
 	if i < m:
 		return 1 + 3 * i
+	# Right of the central sentinel
 	else:
 		return 2 + 3 * (i - m - 1)
 
+# Computes the suffix array for the suffixes starting at indices != 0 mod 3
 def compute_sa_12(string):
 	triplets_12 = []
+	# Create triplets for all indices != 0 mod 3 in the string
 	for i in range(len(string)):
 		if i % 3 != 0:
 			triplet = get_triplet(string, i)
 			triplets_12.append((i,) + triplet)
+	# Sort the triplets
 	triplets_12 = sorted(tuple(triplets_12), key = lambda tup: (tup[1], tup[2], tup[3]))
-	#print("Triplets: ", triplets_12)
+	# Extract the suffix indices
 	sa_12 = [t[0] for t in triplets_12]
 	return sa_12
 
-def construct_isa(sa):
-	isa = {}#[None] * len(sa)
-	for i in range(0, len(sa)):
-		isa[sa[i]] = i
-	return isa
 
-
-def less(string, i, j, isa):
+# Determines which suffix is smaller (i or j), using char comparisons and isa
+def smaller(string, i, j, isa):
+	# Char comparison first
 	a = safe_get_char(string, i)
 	b = safe_get_char(string, j)
 	if a < b:
 		return True
 	if a > b:
 		return False
+	# If true, both suffix i and j will be in sa_12:
+	# We can compare them using isa!
 	if i % 3 != 0 and j % 3 != 0:
 		return isa[i] < isa[j]
-	return less(string, i + 1, j + 1, isa)
+	# Else, we compare i + 1 and j + 1 recursively
+	# We will max get a rec depth of 2, because of the skewed construction
+	return smaller(string, i + 1, j + 1, isa)
 
 
-
+# Merges sa_12 and sa_3 into the final suffix array
 def merge(string, sa_12, sa_3):
+	# Create inverse suffix array for sa_12
     isa_12 = {sa_12[i]: i for i in range(len(sa_12))}
     sa = []
+	# Let i be an index into sa_12 and j be an index into sa_3
     i = j = 0
+	# Check which suffix is smaller: sa_12[i] or sa_3[j]
     while i < len(sa_12) and j < len(sa_3):
-        if less(string, sa_12[i], sa_3[j], isa_12):
+        if smaller(string, sa_12[i], sa_3[j], isa_12):
+			# sa_12[i] is smaller
             sa.append(sa_12[i])
             i += 1
         else:
+			# sa_3[j] is smaller
             sa.append(sa_3[j])
             j += 1
+	# If there are suffixes left in one of the suffix arrays after the other is empty,
+	# add these suffixes to sa
     sa.extend(sa_12[i:])
     sa.extend(sa_3[j:])
     return sa
 
 
-
+# Returns the suffix array of a string
 def skew_rec(string):
-	#print("BEGIN: ", string)
 	sa_12 = compute_sa_12(string)
 	alphabet = get_alphabet(string, sa_12)
-	#print("alphabet: ", alphabet)
-	#print("sa_12: ", sa_12)
 
-	# If all unique in sa_12, don't recurse (if they have equal length, then we have a duplicate in sa_12, because alphabet contains sentinel)
+	# If all unique in sa_12, don't recurse
+	# If the two have equal length, then we have a duplicate in sa_12, because alphabet contains sentinel
 	if len(sa_12) >= len(alphabet):
+		# Compute u string and recurse to find its suffix array
 		u = construct_u(string, alphabet)
-		#print("u string: ", u)
 		sa_u = skew_rec(u)
+		# The index of the central sentinel
 		m = len(u) // 2
-		#print(string, "sa_u: ", sa_u)
+		# Map the indices in sa_u back to indices in the original string
 		sa_12 = [map_u_to_string_index(i, m) for i in sa_u if i != m] # i == m is central sentinel (#)
-		#print(string, m, "sa_12: ", sa_12)
-
 
 	# Construct sa_3 from sa_12
 	sa_3 = []
-	# Special case if last index in string is 0 mod 3, then this suffix should be first in sa_3, since it is the shortest suffix
-	# (it will be sorted based on the first (and in this case only) character later)
+	# Special case: if last index in string is 0 mod 3, then this suffix should be first in sa_3, since it is the shortest suffix
+	# It will be sorted based on the first (and in this case only) character afterwards
 	if len(string) % 3 == 1:
 		sa_3.append(len(string) - 1)
-	# Use the order found in sa_12
+	# All strings mod 3 = 0 are just strings mod 3 = 1, with one char in front, so:
+	# Use the order found in sa_12, and then stable sort on the first character afterwards
 	sa_3 += [i - 1 for i in sa_12 if i % 3 == 1]
 	# Stable sort on first character
-	sa_3 = sorted(sa_3, key = lambda x : string[x][:1])
+	sa_3 = sorted(sa_3, key = lambda i : safe_get_char(string, i)[:1])
 
-	print(string, "sa_12: ", sa_12)
-	print(string, "SA_3: ", sa_3)
+	#print(string, "sa_12: ", sa_12)
+	#print(string, "SA_3: ", sa_3)
 	return merge(string, sa_12, sa_3)
 
 
+
+### TEST CODE ###
+
+s = "mississippi$"
 print(skew_rec(s))
-
-
-
-#print(sorted(["aaa", "aba", "aaa"], key=lambda x:x[0:1]))
 
 
--- suffix_array.py
+++ suffix_array.py
@@ -12,67 +12,6 @@ class suffix_array:
 		self.array = [s[1] for s in sorted((self.string[i:], i) for i in range(self.length))]
 		return self.array
 
-
-	##### SKEW ALGORITHM FOR COMPUTING SUFFIX ARRAY #####
-	def construct_array_skew(self):
-		padded_string = self.string + "$$"
-		sa_12 = self.construct_sa_12(padded_string)
-		self.array = sa_12
-
-	def construct_sa_12(self, string):
-
-		#print(string)
-
-		# Create triples and sort them
-		# [('$$$', 11), ('i$$', 10), ('ipp', 7), ...]
-		suffixes_12 = [s for s in sorted((string[i:i+3], i) for i in range(self.length) if i%3 != 0)]
-
-		# [11, 10, 7, ...]
-		sa_12 = [s[1] for s in suffixes_12]
-		# ['$$$', 'i$$', 'ipp', ...]
-		sa_12_triples =  [s[0] for s in suffixes_12]
-		#print(sa_12_triples)
-
-		# If no duplicates in triple list, we are done
-		if len(sa_12_triples) == len(set(sa_12_triples)):
-			print("DONE")
-			return sa_12
-		else:
-			# Assign new lex names to triples
-			# [(11, '$$$', 0), (10, 'i$$', 1), (7, 'ipp', 2), ...]
-			index_to_lexname_dict = dict([(suffixes_12[i][1], (suffixes_12[i][0], i)) for i in range(len(suffixes_12))])
-
-			# Dict with key: index of suffix into string, and val: (triple, lex name)
-			# {11: ('$$$', 0), 10: ('i$$', 1), 7: ('ipp', 2), ...}
-			lexname = 0
-			index_to_lexname_dict = dict()
-			index_to_lexname_dict[suffixes_12[0][1]] = (suffixes_12[0][0], lexname)
-			for i in range(1, len(suffixes_12)):
-				if suffixes_12[i][0] != suffixes_12[i-1][0]:
-					lexname += 1
-				index_to_lexname_dict[suffixes_12[i][1]] = (suffixes_12[i][0], lexname)
-
-
-			# All the mod 1 suffixes (non-sorted)
-			suffixes_1 = [i for i in range(self.length) if i%3 == 1]
-			# All the mod 2 suffixes (non-sorted)
-			suffixes_2 = [i for i in range(self.length) if i%3 == 2]
-
-			# Create u string to recurse on
-			# 5540#3321  (lex names for mod 2 suffixes, '#', and lex names for mod 1 suffixes)
-			u = ""
-			for i in suffixes_2:
-				u += str(index_to_lexname_dict[i][1])
-			u += "#"
-			for i in suffixes_1:
-				u += str(index_to_lexname_dict[i][1])
-
-			self.construct_sa_12(u)
-
-
-	######################################################
-
-
 	def construct_isa(self):
 		self.isa = [None] * self.length
 		for i in range(0, self.length):
@@ -114,7 +53,6 @@ class lcp_array:
 
 
 sa1 = suffix_array("mississippi")
-sa1.construct_array_skew()
 print(sa1.array)
 
 
