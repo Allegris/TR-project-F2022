e788cb77880f8cb15658cde48c1aa20a4d3a54d6
Allegris <AstridChristiansenCS@gmail.com>
2022-03-07 12:37:52 +0100

    RMQ seems to work (non-indexed still)

--- suffix_array.py
+++ suffix_array.py
@@ -1,6 +1,7 @@
 
 import skew
 import numpy as np
+import sys
 
 ########################################################
 # Class representing a suffix array for a string
@@ -42,6 +43,7 @@ class lcp_array:
 		self.string = sa.string
 		self.length = sa.length
 		self.array = self.construct_lcp()
+		self.RMQ_matrix = self.RMQ_preprocess(len(self.array))
 
 	# Check length of shared prefix between suffix i and j
 	def compare_lcp(self, string, i, j):
@@ -88,43 +90,49 @@ class lcp_array:
 		res.append((prev_i, j))
 		return res
 
-########################################################
-# Range Minimum Query (RMQ)
-########################################################
+	########################################################
+	# Range Minimum Query (RMQ) for LCP array
+	########################################################
+
+	# TODO: Remember the indices in the return
+
 	'''
 	Computes the range minimum query (RMQ) of interval [i,j), i.e. leftmost occurrence of min value in range [i,j)
 	Returns RMQ of the form (index, min_value)
 	'''
-	def RMQ(self, lcp, i, j):
-		M = RMQ_preprocess(len(lcp))
-		return i, 0
+	def RMQ(self, L, R):
+		interval = self.array[L:R]
+		j = len(interval).bit_length() - 1 # log(interval_len) floored
+		# Min of left and right interval of exponents 2^j
+		# I.e. interval starting at pos L with length 2^j
+		# and interval starting at pos R - 2**j of length 2^j
+		return min(self.RMQ_matrix[L][j], self.RMQ_matrix[R - (2**j)][j])
+		#return 0, 0
 
-	def RMQ_preprocess(n):
-
+	# Preprocess matrix for RMQ in time O(n*log(n))
+	def RMQ_preprocess(self, n):
 		# M matrix to fill: n x log(n) (floored),
 		# where entry M[i][j] is the RMQ for interval starting at idx i of length 2^j
 		log_n = n.bit_length() - 1 # this is log(n) floored, so eg. for 15 => 3 (2^3 = 8)
-		M = np.empty((n, log_n))
-
-		# Intervals of length 1
+		M = np.full((n, log_n + 1), fill_value = 1000000, dtype = int) # +1 is the first column of singleton intervals
+		# Intervals of length 1 first:
 		for i in range(n):
-			M[i][0] = lcp[i]
+			M[i][0] = self.array[i]
 
-		# Preprocessing/filling out M for intervals of length 2^k (2, 4, 8, 16...)
-
-
+		# Preprocessing/filling out M for intervals of length 2^k (2, 4, 8, 16...) next:
 		# Run through all smaller exponents
 		# So if we want to know RMQ of a 2^k, then we run through j = 1,2,3,...,k-1
-		for j in range(1, log_n - 1):
+		for j in range(1, log_n + 1):
 			# Run through all intervals of length 2**j (stop if interval exceeds list length)
-			last_idx = (n - 2**j) + 1 # last starting idx in list where interval does not exceed list length
+			last_idx = (n - (2**j)) + 1 # last starting idx in list where interval does not exceed list length
 			for i in range(last_idx):
 				# Every interval of length 2^j can be seen as two intervals of size 2^(j-1)
 				# So for an interval of size 2^j, take the minimum of the two 2^(j-1) values
 				# These two are the intervals:
 				# 1) Starting at pos i with length 2^(j-1)
 				# 2) Starting at pos i + 2^(j-1) with length 2^(j-1)
-				M[i][j] = min(M[i][j-1], M[i+2**(j-1)][j-1])
+				M[i][j] = min(M[i][j-1], M[i+(2**(j-1))][j-1])
+		return M
 
 
 
@@ -141,6 +149,9 @@ print("sa:  ", sa1.array)
 
 sa1.construct_lcp_array()
 print("lcp: ", sa1.lcp.array)
+
+print(sa1.lcp.RMQ(11,12))
+#print(sa1.lcp.RMQ_matrix)
 
 
 
