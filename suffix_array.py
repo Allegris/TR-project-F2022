2d994c12c8a3a871123c5f4b66b040e6f6e5beea
Allegris <AstridChristiansenCS@gmail.com>
2022-03-07 14:28:51 +0100

    RMQ done, I think

--- suffix_array.py
+++ suffix_array.py
@@ -94,44 +94,56 @@ class lcp_array:
 	# Range Minimum Query (RMQ) for LCP array
 	########################################################
 
-	# TODO: Remember the indices in the return
-
 	'''
-	Computes the range minimum query (RMQ) of interval [i,j), i.e. leftmost occurrence of min value in range [i,j)
+	Computes the range minimum query (RMQ) of interval [i,j), i.e. (index and value of)
+	leftmost occurrence of min value in range [i,j)
 	Returns RMQ of the form (index, min_value)
 	'''
 	def RMQ(self, L, R):
-		interval = self.array[L:R]
-		j = len(interval).bit_length() - 1 # log(interval_len) floored
+		interval = self.array[L:R] # The interval to do RMQ on
+		j = len(interval).bit_length() - 1 # log(interval_len) floor
+		# RMQ matrix has the form [[(idx, val), (idx, val),...],[(idx, val),...],...]
+		# Where rows are LCP indices and
+		# Cols are j=1,2,4,8,16,... where we have calculated RMQ for intervals of lengths 2^j
+		# So col 1 is j=0 (interval length 2^j = 2^0 = 1), col 2 is j=1 (interval length 2^1=2), etc.
+
 		# Min of left and right interval of exponents 2^j
 		# I.e. interval starting at pos L with length 2^j
 		# and interval starting at pos R - 2**j of length 2^j
-		return min(self.RMQ_matrix[L][j], self.RMQ_matrix[R - (2**j)][j])
-		#return 0, 0
+		# There may be an overlap in the two intervals, but this is OK, result will not change
+		if self.RMQ_matrix[L][j][1] <= self.RMQ_matrix[L + (2**j)][j][1]:
+			return self.RMQ_matrix[L][j]
+		else:
+			return self.RMQ_matrix[L + (2**j)][j]
 
-	# Preprocess matrix for RMQ in time O(n*log(n))
+
+	'''
+	Preprocess matrix for RMQ in time O(n*log(n))
+	'''
 	def RMQ_preprocess(self, n):
-		# M matrix to fill: n x log(n) (floored),
+		# M matrix to fill: n x log(n),
 		# where entry M[i][j] is the RMQ for interval starting at idx i of length 2^j
-		log_n = n.bit_length() - 1 # this is log(n) floored, so eg. for 15 => 3 (2^3 = 8)
-		M = [[sys.maxsize]*(log_n + 1) for _ in range(n)]
+		log_n = n.bit_length() # this is log(n) ceil, so eg. for 15 => 4 (2^4 = 16)
+		M = [[sys.maxsize]*(log_n) for _ in range(n)]
 		# Intervals of length 1 first:
 		for i in range(n):
-			M[i][0] = self.array[i]
+			M[i][0] = (i, self.array[i])
 
 		# Preprocessing/filling out M for intervals of length 2^k (2, 4, 8, 16...) next:
-		# Run through all smaller exponents
-		# So if we want to know RMQ of a 2^k, then we run through j = 1,2,3,...,k-1
-		for j in range(1, log_n + 1):
+		# Run through all exponents, in increasing order: j = 1,2,4,8,..., log n
+		for j in range(1, log_n):
 			# Run through all intervals of length 2**j (stop if interval exceeds list length)
-			last_idx = (n - (2**j)) + 1 # last starting idx in list where interval does not exceed list length
-			for i in range(last_idx):
+			last_idx = (n - (2**j)) + 1
+			for i in range(last_idx): # note: last_index excluded
 				# Every interval of length 2^j can be seen as two intervals of size 2^(j-1)
 				# So for an interval of size 2^j, take the minimum of the two 2^(j-1) values
 				# These two are the intervals:
 				# 1) Starting at pos i with length 2^(j-1)
 				# 2) Starting at pos i + 2^(j-1) with length 2^(j-1)
-				M[i][j] = min(M[i][j-1], M[i+(2**(j-1))][j-1])
+				left_val = M[i][j-1][1]
+				right_idx = i+(2**(j-1))
+				right_val = M[right_idx][j-1][1]
+				M[i][j] = (i, left_val) if left_val <= right_val else (right_idx, right_val)
 		return M
 
 
@@ -150,9 +162,8 @@ print("sa:  ", sa1.array)
 sa1.construct_lcp_array()
 print("lcp: ", sa1.lcp.array)
 
-print(sa1.lcp.RMQ(11,12))
+print(sa1.lcp.RMQ(3, 6))
 print(DataFrame(sa1.lcp.RMQ_matrix))
-
 
 
 
