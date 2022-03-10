5f494033eabf1b238e9900c1f9cb178fe099252e
Allegris <AstridChristiansenCS@gmail.com>
2022-03-10 13:09:24 +0100

    Smaller half trick works!

--- suffix_array.py
+++ suffix_array.py
@@ -125,8 +125,63 @@ class lcp_array:
 	# Finding branching tandem repeats
 	########################################################
 
+	'''
+	Returns the widest interval (i, j) from a list of intervals
+	'''
+	def widest(self, intervals):
+		max_size = 0
+		max_interval = (None, None)
+		for (i, j) in intervals:
+			if j - i > max_size:
+				max_size = j - i
+				max_interval = (i, j)
+		return max_interval
 
 	'''
+	Finds all branching tandem repeats using the "smaller half trick" to get a
+	running time of O(n log(n)) for a string of length n
+	Returns all branching tandem repeats in a list [(string_idx, L)]
+	Where L is the length of the repeated substring, so the tandem repeat "AA" has length 2*L
+	'''
+	def branching_TR_smaller_half(self, i, j):
+		res = []
+		# Suffix array and inverse suffix array
+		sa = self.sa.array
+		isa = self.isa
+		# All inner nodes in suffix tree
+		inner_nodes = [(i, j) for (i, j) in self.child_intervals_rec(i, j) if j - i > 1]
+		# For each inner node v, check if each leaf below is a tandem repeat:
+		# A leaf is a tandem repeat if isa[sa[leaf] + depth(v)] is below v,
+		# but in a different subtree than leaf.
+		for (node_i, node_j) in inner_nodes:
+			child_intervals = self.child_intervals_rec(node_i, node_j)
+			# The widest subtree/interval
+			(w_i, w_j) = self.widest(child_intervals)
+			# L is the shared node depth for all children in this subtree (i.e. child interval):
+			(_, L) = self.RMQ(node_i+1, node_j)
+			# Run through each subtree, EXCEPT THE WIDEST SUBTREE
+			for (ii, jj) in child_intervals:
+				if (ii, jj) == (w_i, w_j):
+					continue
+				# Run through each leaf in subtree
+				for q in range(ii, jj):
+					# *** Check to the right ***
+					# "Leaf sa[q]+L" that may potentially form a branching TR with "leaf sa[q]"
+					r = isa[sa[q] + L]
+					# If "leaf sa[q]+L" is in a different subtree
+					# than "leaf sa[q]", then we have a branching TR:
+					if r in range(node_i, ii) or r in range(jj, node_j):
+						res.append((sa[q], L))
+					# *** Check to the left (widest subtree) ***
+					r = isa[sa[q] - L]
+					# If "leaf sa[q]-L" is in the widest subtree, then we would have missed it,
+					# since we don't run through the widest subtree, so we add it here
+					if r in range(w_i, w_j):
+						res.append((sa[r], L))
+		return list(set(res)) # remove duplicates
+
+	'''
+	Finds all branching tandem repeats in running time O(n^2) for a string of length n
 	Returns all branching tandem repeats in a list [(string_idx, L)]
 	Where L is the length of the repeated substring, so the tandem repeat "AA" has length 2*L
 	'''
@@ -153,7 +208,7 @@ class lcp_array:
 					# If "Leaf sa[q]+L" is in a different subtree
 					# than "leaf sa[q]", then we have a branching TR:
 					if r in range(node_i, ii) or r in range(jj, node_j):
-						res.append((sa[r], L))
+						res.append((sa[q], L))
 		return list(set(res)) # remove duplicates
 
 
@@ -226,5 +281,8 @@ print("lcp: ", lcp.array)
 #print("Child intervals: ", lcp.get_child_intervals(0, 9))
 #print("All child intervals (recursive): ", lcp.child_intervals_rec(0, 12))
 print("Branding tandem repeats: ", lcp.branching_TR(0, 12))
+print("Branding tandem repeats SMALLER HALF: ", lcp.branching_TR_smaller_half(0, 12))
+
+
 
 
